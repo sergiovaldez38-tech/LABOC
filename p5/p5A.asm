4e695862db20a3c2a2167c98c1c185bbc2dc588a
sergiovaldez38-tech <sergio.valdez38@uabc.edu.mx>
2025-09-30 18:45:58 +0000

    P5

---
+++ p5/p5A.asm
@@ -0,0 +1,20 @@
+%include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimiento externos
+								; que se encuentran en la biblioteca libpc_io.a
+
+section	.text
+	global _start       ;referencia para inicio de programa
+	
+_start:                   
+	mov edx, msg		; edx = dirección de la cadena msg
+	call puts			; imprime cadena msg terminada en valor nulo (0)
+
+	    	; seleccionar llamada al sistema para fin de programa
+	mov byte [msg], 'Z' 
+	mov edx, msg
+	
+	call puts
+	mov eax, 1
+	int 0x80      	; llamada al sistema - fin de programa
+section	.data
+msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+
