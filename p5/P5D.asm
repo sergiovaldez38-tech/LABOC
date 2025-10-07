3d9afe933aaee316f1214a45900ca7cc8fb86d30
sergiovaldez38-tech <sergio.valdez38@uabc.edu.mx>
2025-10-06 23:27:57 -0700

    P5

--- p5/P5D.asm
+++ p5/P5D.asm
@@ -1,4 +1,5 @@
-%include "../LIB/pc_io.inc"
+%include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimiento externos
+								; que se encuentran en la biblioteca libpc_io.a
 
 section	.text
 	global _start
@@ -7,8 +8,9 @@ _start:
 	mov edx, msg
 	call puts
 
-	mov ebx, msg     ; base
-	mov esi, 25      ; índice -> posición de 'z'
+	; Reemplazar 'z' (posición 25) por 'Z' usando base + índice
+	mov ebx, msg
+	mov esi, 25
 	mov byte [ebx + esi], 'Z'
 
 	mov edx, msg
