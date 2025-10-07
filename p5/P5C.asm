6e4ff09d0a5cddbe64b1719d9af4f58277c6e095
sergiovaldez38-tech <sergio.valdez38@uabc.edu.mx>
2025-10-06 23:24:51 -0700

    PS5

--- p5/P5C.asm
+++ p5/P5C.asm
@@ -1,23 +1,25 @@
-%include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimientos externos
+%include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimiento externos
 								; que se encuentran en la biblioteca libpc_io.a
 
 section	.text
-	global _start       		; referencia para inicio de programa
+	global _start       ; referencia para inicio de programa
 	
 _start:
-	mov edx, msg        		; edx = dirección de la cadena msg
-	call puts					; imprime cadena original
+	mov edx, msg		; edx = dirección de la cadena msg
+	call puts			; imprime cadena original
 
-	; Reemplazo usando direccionamiento relativo a registro
-	mov ebx, msg        		; ebx = dirección base de msg
-	add ebx, 26         		; mover a la posición del carácter '0'
-	mov byte [ebx], '@' 		; reemplaza el '0' por '@'
+	; Reemplazar el número 0 (posición 26) por el símbolo @
+	mov ebx, msg        ; base de la cadena
+	add ebx, 26         ; desplazamiento relativo a registro
+	mov byte [ebx], '@' ; reemplazo
 
-	mov edx, msg        		; mostrar cadena modificada
+	mov edx, msg		; mostrar la cadena modificada
 	call puts
 
-	mov eax, 1					; llamada al sistema para terminar programa
+	mov eax, 1			; fin del programa
 	int 0x80
 
 section	.data
 msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0
+
+
